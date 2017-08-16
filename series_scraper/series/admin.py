7424ed6ea13e5c2883592de9cd3f2becc14b32ce
teenoh <valentino@grit.systems>
2017-08-16 06:01:18 +0100

    updated models

--- series_scraper/series/admin.py
+++ series_scraper/series/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Series, Seasons, Episodes
 # Register your models here.
+
+admin.site.register(Series)
--- series_scraper/series/models.py
+++ series_scraper/series/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Series(models.Model):
+    name = models.CharField(max_length=120)
+    url = models.URLField()
+    image_url = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+class Seasons(models.Model):
+    name = models.CharField(max_length=20)
+    url = models.URLField
+    series = models.ForeignKey(Series, on_delete=models.Cascade)
+
+    def __str__(self):
+        return self.name
+
+class Episodes(models.Model):
+    name = models.CharField(max_length=20)
+    download_link = models.URLField()
+
