1c7edc88cd1b399092b9733e9781fb8cd1b9760e
teenoh <valentino@grit.systems>
2017-08-18 04:27:08 +0100

    edited tasks

--- series_scraper/series/tasks.py
+++ series_scraper/series/tasks.py
@@ -19,19 +19,29 @@ def update_db():
         data = json.load(fp)
         fp.close()
     for series, series_details in data.items():
-        ser = Series.objects.create(name=series)
-        ser.save()
-        print("updated {ser}'s name".format(ser=ser.name))
+        ser = Series.objects.get(name=series)
+        if not ser:
+            ser = Series.objects.create(name=series)
+            ser.save()
+            print("updated {ser}'s name".format(ser=ser.name))
+
         for season, season_details in series_details.items():
-            sea = Seasons.objects.create(name=season, series=ser)
-            sea.save()
-            print("updated {ser}-{sea} name".format(ser=ser.name, sea=sea.name))
+            sea = Seasons.objects.get(name=season, series=ser)
+            if not sea:
+                sea = Seasons.objects.create(name=season, series=ser)
+                sea.save()
+                print("updated {ser}-{sea} name".format(ser=ser.name, sea=sea.name))
+
             for episode, episode_details in season_details.items():
-                epis = Episodes.objects.create(name=episode,
+                epis = Episodes.objects.get(name=episode,
                                                download_link=episode_details,
                                                season=sea)
-                epis.save()
-                print("updated {ser}-{sea}-{epis} name".format(ser=ser.name,
+                if not epis:
+                    epis = Episodes.objects.create(name=episode,
+                                                   download_link=episode_details,
+                                                   season=sea)
+                    epis.save()
+                    print("updated {ser}-{sea}-{epis} name".format(ser=ser.name,
                                                                sea=sea.name,
                                                                epis=epis.name))
     print("completed baby")
