b51fd6848f27113d8c47d2c8a6278b690d0a69a1
teenoh <valentino@grit.systems>
2017-08-18 06:56:04 +0100

    updated series_viewsets

--- series_scraper/series/views.py
+++ series_scraper/series/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import generics
 
+from .serializers import SeriesSerializer
+from .models import Series, Seasons, Episodes
 # Create your views here.
+
+class SeriesViewSet(viewsets.ModelViewSet):
+    '''
+    This si the viewset for the Series model
+    '''
+    queryset = Series.objects.all()
+    serializer_class = SeriesSerializer
+
