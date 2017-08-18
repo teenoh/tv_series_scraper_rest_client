1581cd02b0faaacea1ad1103f8cf0b82bc1536a0
teenoh <valentino@grit.systems>
2017-08-18 04:48:29 +0100

    changed config

--- series_scraper/series_scraper/celery.py
+++ series_scraper/series_scraper/celery.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 import os
 from celery import Celery
 from django.conf import settings
@@ -5,9 +6,10 @@ from django.conf import settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'series_scraper.settings')
 app = Celery('series_scraper')
 
-# Using a string here means the worker will not have to
-# pickle the object when using Windows.
-app.config_from_object('django.conf:settings')
+# Using a string here means the worker don't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
