3798a7ff1ad72abbd5bed6be339ca3cd856b9202
teenoh <valentino@grit.systems>
2017-08-18 06:18:06 +0100

    drf

--- series_scraper/series_scraper/settings.py
+++ series_scraper/series_scraper/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'series',
     'django_celery_beat',
-    'django_celery_results'
+    'django_celery_results',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
