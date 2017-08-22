f903b37f959d60fcfe5ea850fba26aef212a77fd
teenoh <valentino@grit.systems>
2017-08-22 10:36:21 +0100

    rearranged directory

--- series_scraper/series/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Series, Seasons, Episodes
-# Register your models here.
-
-admin.site.register(Series)
-admin.site.register(Seasons)
-admin.site.register(Episodes)
