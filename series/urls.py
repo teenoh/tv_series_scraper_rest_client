73f4b09210113c4ff6736db241d7911c6f68349f
teenoh <valentino@grit.systems>
2017-08-29 08:22:40 +0100

    .

--- series/urls.py
+++ series/urls.py
@@ -10,10 +10,10 @@ router.register(r'episodes', EpisodeViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^all_series/$', SeriesViewSet.as_view({'get': 'list'}), name="detail"),
-    url(r'^all_series/(?P<series_name>.+)/$', SeasonViewSet.as_view({'get': 'list'})),
-    url(r'^all_series/(?P<series_name>.+)/(?P<season_name>.+)/', SeasonViewSet.as_view({'get': 'detail'}))
-    url(r'^(?P<series>.+)/(?P<season>.+)/(?P<episode>.+)/', index)
+    #url(r'^all_series/$', SeriesViewSet.as_view({'get': 'list'}), name="detail"),
+    #url(r'^all_series/(?P<series_name>.+)/$', SeasonViewSet.as_view({'get': 'list'})),
+    #url(r'^all_series/(?P<series_name>.+)/(?P<season_name>.+)/', SeasonViewSet.as_view({'get': 'detail'})),
+    url(r'^(?P<series_name>.+)/(?P<season_name>.+)/(?P<episode_name>.+)/', index)
     #url('^series/(?P<series_name>.+)', SeriesList.as_view(), name="series_list"),
     # url('^series/(?P<series_name>.+)/(?P<season_name>.+)/$', SeasonList.as_view(), name="season_list")
 ]
